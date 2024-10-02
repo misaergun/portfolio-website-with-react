d3bbfd437fc4fa6469e3643177600ca20e4e8d3f
M. Nisa Ergün <nisaergun.21@gmail.com>
2024-10-02 19:26:30 +0300

    Made social media buttons clickable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import logo from "../assets/nisaLogo.png"
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaMedium } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import logo from "../assets/nisaLogo.png";
+import { FaLinkedin, FaGithub, FaMedium, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-    return <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-20"src={logo} alt="logo" />
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-            <FaMedium />
-            <FaInstagram />
-        </div>
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-20" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/m%C3%BCberra-nisa-erg%C3%BCn-856a0923b/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/misaergun" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://medium.com/@nisaergun.21" target="_blank" rel="noopener noreferrer">
+          <FaMedium />
+        </a>
+        <a href="https://www.instagram.com/misaergun/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      </div>
     </nav>
+  );
 };
 
 export default Navbar;
-
