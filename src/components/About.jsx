bce193c1a0f519168e7e946dc273b89f09b2262c
M. Nisa Ergün <nisaergun.21@gmail.com>
2024-10-02 18:03:29 +0300

    feat: Add animations using Framer Motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import aboutImg from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
+
 const About = () => {
     return (
         <div className="border-b border-neutral-900 pb-4">
@@ -7,16 +9,24 @@ const About = () => {
                 <span className="text-pink-900"> Me</span>
             </h1>
             <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2 lg:p-7">
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.5 }}
+                    className="w-full lg:w-1/2 lg:p-7">
                     <div className="flex items-center justify-center">
-                        <img src={aboutImg} alt="about" className=" rounded-lg"/>
+                        <img src={aboutImg} alt="about" className=" rounded-lg" />
                     </div>
-                </div>
-                <div className="w-full lg:w-1/2">
-                        <div className="flex justify-center lg:justify-start">
-                            <p className="my-2 maxw-xl py-6">{ABOUT_TEXT}</p>
-                        </div>
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 0.5 }}
+                    className="w-full lg:w-1/2">
+                    <div className="flex justify-center lg:justify-start">
+                        <p className="my-2 maxw-xl py-6">{ABOUT_TEXT}</p>
                     </div>
+                </motion.div>
             </div>
         </div>
     )
