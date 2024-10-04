3c76dd386f59e394d4db3362f07827ee67220663
M. Nisa Ergün <nisaergun.21@gmail.com>
2024-10-04 18:02:26 +0300

    Added video part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
 const Footer = () => {
     return (
-        <footer className="bg-neutral-950 text-neutral-300 py-6">
-            <div className="container mx-auto text-center">
-                <p>&copy; 2024 Müberra Nisa Ergün. All rights reserved.</p>
+        <div>
+            {/* Video part */}
+            <div className="flex flex-col items-center mb-6">
+                <p className="mb-4 text-center text-neutral-400">
+                    Finally, to brighten your day, I added a cat video. Thank you for visiting my website!
+                </p>
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/7Nn7NZI_LN4?autoplay=0" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
+                </iframe>
             </div>
-        </footer>
+
+            {/* Footer part */}
+            <footer className="bg-neutral-950 text-neutral-300 py-6">
+                <div className="container mx-auto text-center">
+                    <p>&copy; 2024 Müberra Nisa Ergün. All rights reserved.</p>
+                </div>
+            </footer>
+        </div>
     );
 };
 
